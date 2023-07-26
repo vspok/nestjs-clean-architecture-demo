d3d3fbdd48e60e9d4c3ce7c6be6c6d63701a5702
Vitor Dias <vitorgabrielgarselaz@gmail.com>
2023-07-25 22:06:24 -0300

    ajuste

--- src/infra/http/controllers/user.controller.ts
+++ src/infra/http/controllers/user.controller.ts
@@ -7,13 +7,14 @@ import { CreateUserBody } from '../dtos/create-user-body';
 import { AuthGuard } from 'src/infra/guards/auth.guard';
 import { UpdateUser } from 'src/application/use-cases/user/update-user';
 import { UpdateUserBody } from '../dtos/update-user-body';
+import { FindManyUser } from 'src/application/use-cases/user/find-many-user';
 
 @Controller('user')
 @ApiTags('user')
 @ApiResponse({ status: 500, description: 'Internal error' })
 @ApiExtraModels(UserViewModel)
 export class UsersController {
-    constructor(private createUser: CreateUser, private updateUser: UpdateUser) {}
+    constructor(private createUser: CreateUser, private updateUser: UpdateUser, private findManyUser: FindManyUser) {}
 
     @UseGuards(AuthGuard)
     @ApiOperation({ summary: 'create User' })
@@ -29,5 +30,13 @@ export class UsersController {
     async update(@Param('user_id') user_id: string,@Body() body: UpdateUserBody) {
         const { userUpdated } = await this.updateUser.execute({user_id: +user_id, user: body});
         return new UserViewModel(userUpdated);
+    }
+    @UseGuards(AuthGuard)
+    @ApiOperation({ summary: 'FindMany User' })
+    @Get('')
+    async findMany(@Query() filter) {
+        const { userPage } = await this.findManyUser.execute({filter});
+        // return new UserViewModel(userUpdated);
+        return userPage;
     }
 }
